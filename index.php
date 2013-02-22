b65320918276069a746bf167f75eb97594c5ecf3
cfphpflex <cfphpflex@gmail.com>
2013-02-21 17:08:47 -0800

    commit 2
    'cause I can

---
+++ index.php
@@ -0,0 +1,15 @@
+  // Show a custom alert
+    //
+    function showAlert() {
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
+    
+  	
+  
+	</script>
+	
