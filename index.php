7cfad7695bd553d294acf3d938fa5890ba5fe9a9
cfphpflex <cfphpflex@gmail.com>
2013-02-21 17:13:59 -0800

    commit
    comment

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-   //CF9 cfscript garbage collector
+   //CF9 cfscript garbage collector  // tee hee u know this php won't work; right
    
    	obj = createObject("java", "java.lang.System");
 	obj.gc();
